8c30ff167a199a84367bdb85e65fc71852745d51
法外狂徒张三 <46189169+kelecn@users.noreply.github.com>
2020-11-21 15:20:29 +0800

    Update script.js

--- simple/script.js
+++ simple/script.js
@@ -40,28 +40,3 @@ jQuery(document).ready(function() {
 
 	});
 });
-
-if (isMobile) {
-    donate_buttons.addClass('Mobile');
-}
-
-icon_donate.on('click',drinks_an[0]); // drinks 图标点击
-
-donate_button_bg.on('click',drinks_an[1]); // 隐藏 donate box
-
-donate_button.on('click',function(){
-    var thisID = $(this).attr("id");
-    if (isMobile && thisID === 'alipay_donate') {
-        // 当前网页在手机端打开跳转到支付宝 App
-        window.open(qrcodes['alipay_donate_link']);
-    } else {
-        // 当前网页在PC端打开
-    drinks_qrcode.css({'background-image' : 'url('+qrcodes[thisID]+')'});
-    drinks_an[2]();
-    // 显示二维码
-    }
-});
-
-drinks_qrcode.on('click',drinks_an[3]); // 隐藏二维码
-//
-})
