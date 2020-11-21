2737ca01b09897dd611fd669bba260375146bfc4
法外狂徒张三 <46189169+kelecn@users.noreply.github.com>
2020-11-21 20:41:02 +0800

    Update script.js

--- drinks/script.js
+++ drinks/script.js
@@ -11,8 +11,8 @@ var drinks_qrcodes = $('#drinks-qrcodes');
 var drinks_qrcode = $('#drinks-qrcode');
 var isMobile = /Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent);
 
-var GithubLink  =   "https://github.com/Kaiyuan/donate-page/";
-var PayPalLink  =   "https://www.paypal.me/KaiyuanXie";
+var GithubLink  =   "https://github.com/kelecn/sponsor-page/";
+var PayPalLink  =   "https://www.paypal.me/kelecn";
 $('#paypal-donate>a').href = PayPalLink;
 $('#github-box>a').href = GithubLink;
 var qrcodes = {
